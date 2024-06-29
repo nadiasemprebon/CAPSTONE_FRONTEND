f1d919bab0b4df767383d4ccd9486bc19b4c58ab
nadiasemprebon <semprebonnadia6@gmail.com>
2024-06-29 05:49:53 -0700

    commit 3 con log outfunzionante

--- DiVino/src/app/components/navbar/navbar.component.ts
+++ DiVino/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  show:boolean = false
+  isUserLoggedIn:boolean = false;
+
+  constructor(private authSvc:AuthService){}
+
+  ngOnInit(){
+
+    this.authSvc.isLoggedIn$.subscribe(data => {
+
+      this.isUserLoggedIn = data;
+
+    })
+
+  }
+
+  logout(){
+    this.authSvc.logout()
+  }
 
 }
