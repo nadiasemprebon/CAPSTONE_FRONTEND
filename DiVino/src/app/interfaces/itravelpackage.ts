389928d4144cfbd888de6103c03dbb9066a8518f
nadiasemprebon <semprebonnadia6@gmail.com>
2024-07-09 11:47:13 -0700

    commit img

--- DiVino/src/app/interfaces/itravelpackage.ts
+++ DiVino/src/app/interfaces/itravelpackage.ts
@@ -1,9 +1,6 @@
-import { IWinery } from "./iwinery";
-
-
 export interface ITravelpackage {
-  id:number,
-  wineryId: IWinery;
+  id: number;
+  wineryId: number; // Assicurati che sia un numero
   travelPackageName: string;
   imageUrl?: string;
   startDate: Date;
