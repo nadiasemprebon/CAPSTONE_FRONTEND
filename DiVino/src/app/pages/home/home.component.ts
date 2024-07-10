416f71619f3fe025230440ce0fcc5975f779f475
nadiasemprebon <semprebonnadia6@gmail.com>
2024-07-10 12:17:53 -0700

    delete che funziona

--- DiVino/src/app/pages/home/home.component.ts
+++ DiVino/src/app/pages/home/home.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { TravelpackageServiceService } from '../travelpackages/travelpackage-service.service';
 import { ITravelpackage } from '../../interfaces/itravelpackage';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'] // Correzione: 'styleUrl' dovrebbe essere 'styleUrls'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   travelpackages: ITravelpackage[] = [];
+  private subscriptions: Subscription = new Subscription();
 
   constructor(private travelpackageService: TravelpackageServiceService) {}
 
@@ -20,9 +22,23 @@ export class HomeComponent implements OnInit {
     this.travelpackageService.getAll().subscribe();
   }
 
-  deletePackage(packageId: number): void {
-    this.travelpackageService.delete(packageId).subscribe(() => {
-      this.travelpackages = this.travelpackages.filter(p => p.id !== packageId);
+  deletePackage(id: number) {
+    console.log('ID da cancellare:', id, 'Tipo:', typeof id); // Log ID e tipo
+
+    if (!id) {
+      console.error('ID non valido:', id);
+      return;
+    }
+
+    const deleteTravelpackageSub = this.travelpackageService.delete(id).subscribe({
+      next: () => {
+        this.travelpackages = this.travelpackages.filter(product => product.id !== id);
+        this.subscriptions.add(deleteTravelpackageSub);
+        console.log('Pacchetto cancellato, nuovo array:', this.travelpackages); // Log nuovo array
+      },
+      error: (error) => {
+        console.error('Errore nella cancellazione del prodotto', error);
+      }
     });
   }
 }
--- DiVino/src/app/pages/travelpackages/travelpackage-service.service.ts
+++ DiVino/src/app/pages/travelpackages/travelpackage-service.service.ts
@@ -9,8 +9,8 @@ import { environment } from '../../../environments/environment.development';
   providedIn: 'root'
 })
 export class TravelpackageServiceService {
-  travelpackage: ITravelpackage[] = [];
-  travelpackageSubject = new BehaviorSubject<ITravelpackage[]>([]);
+  private travelpackage: ITravelpackage[] = [];
+  private travelpackageSubject = new BehaviorSubject<ITravelpackage[]>([]);
   travelpackage$ = this.travelpackageSubject.asObservable();
 
   constructor(private http: HttpClient, private authSvc: AuthService) {
@@ -29,10 +29,10 @@ export class TravelpackageServiceService {
   }
 
   getById(id: number): ITravelpackage | null {
-    return this.travelpackage.find(u => u.id == id) || null;
+    return this.travelpackage.find(u => u.id === id) || null;
   }
 
-  update(travel: ITravelpackage) {
+  update(travel: ITravelpackage): Observable<ITravelpackage> {
     const formData = new FormData();
     formData.append('wineryId', travel.wineryId.toString());
     formData.append('travelPackageName', travel.travelPackageName);
@@ -50,36 +50,36 @@ export class TravelpackageServiceService {
     };
 
     return this.http.put<ITravelpackage>(`${this.apiUrl}/${travel.id}`, formData, httpOptions).pipe(
-      tap(trav => {
-        const index = this.travelpackage.findIndex(u => u.id == travel.id);
-        this.travelpackage.splice(index, 1, trav);
+      tap(updatedTravel => {
+        const index = this.travelpackage.findIndex(u => u.id === travel.id);
+        this.travelpackage[index] = updatedTravel;
         this.travelpackageSubject.next(this.travelpackage);
       })
     );
   }
 
-  create(formData: FormData) {
+  create(formData: FormData): Observable<ITravelpackage> {
     const httpOptions = {
       headers: new HttpHeaders({
         Authorization: `Bearer ${this.authSvc.getAccessToken()}`
       })
     };
 
-    return this.http.post<ITravelpackage>(this.apiUrl, formData, httpOptions).pipe(
-      tap(trav => {
-        this.travelpackage.push(trav);
+    return this.http.post<ITravelpackage>(this.apiUrl, formData).pipe(
+      tap(newTravel => {
+        this.travelpackage.push(newTravel);
         this.travelpackageSubject.next(this.travelpackage);
       })
     );
   }
 
-  delete(id: number) {
-    return this.http.delete<ITravelpackage>(`${this.apiUrl}/${id}`).pipe(
-      tap(() => {
-        const index = this.travelpackage.findIndex(u => u.id == id);
-        this.travelpackage.splice(index, 1);
-        this.travelpackageSubject.next(this.travelpackage);
+  delete(id: number): Observable<void> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${this.authSvc.getAccessToken()}`
       })
-    );
+    };
+
+    return this.http.delete<void>(`${this.apiUrl}/${id}`, httpOptions);
   }
 }
